3ac94a67d0b7603b942b10cefa128a825abe0257
Aaditya Pal <ninjaaadi@Aadityas-MacBook-Pro.local>
2022-02-24 20:06:32 +0530

    deployement

--- valitators/check_valid_value.js
+++ valitators/check_valid_value.js
@@ -24,6 +24,7 @@ exports.validDate = (date) => {
 
 //Validator functions to validate a url
 exports.isUrlValid = (userInput) => {
+  if (userInput == null) return false;
   var res = userInput.match(
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
